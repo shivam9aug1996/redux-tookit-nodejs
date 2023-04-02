4f225fa456206a78d4db1aa57bb0134745ea2812
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2023-04-02 11:56:55 +0530

    fied bugs

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ import { keysProd } from "./prod.js";
 import { keysDev } from "./dev.js";
 let key;
 if (process.env.NODE_ENV == "production") {
-  key = keysProd;
+  key = keysDev;
 } else {
   key = keysDev;
 }
