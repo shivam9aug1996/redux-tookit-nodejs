28da8de5cbdefddff536b2f4ea3b0df89e23b917
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2023-04-02 12:02:14 +0530

    fied bugs

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+export const keysDev = {
+  JWT_SECRET: "secretkey",
+  MONGO_URI:
+    "mongodb+srv://shivam9aug1996:VXKC4yT60DtAAte4@cluster0.nt9xusc.mongodb.net/?retryWrites=true&w=majority",
+};
