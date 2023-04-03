030b7aa74457a99bdb0616ad8257c9f6f0cd1897
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2023-04-03 07:30:53 +0530

    fied bugs

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post("/signin", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
